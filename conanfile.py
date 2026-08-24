63eede05f8096a5b2436812213b894e69cd7f412
Daryl Stark <daryl@dstark.nl>
2026-08-24 18:03:40 +0000

    chore: Update local files.

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+import os
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
+
+
+class SabreAppPilotConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    
+    def requirements(self):
+        skips = os.getenv("CONAN_SKIPS", default='').split()
+        requires = (
+            "sfml/3.0.2",
+        )
+        
+        for requirement in requires:
+            if requirement.split('/')[0] in skips:
+                continue
+            self.requires(requirement)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        CMakeDeps(self).generate()
+        CMakeToolchain(self).generate()
